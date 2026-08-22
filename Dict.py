84ac72dac3fd1a09f6a2b83eecf5832fea5f50dc
Aditya yadav <adityayadav76430@gmail.com>
2026-08-22 12:37:41 +0530

    dict menu

---
+++ Dict.py
@@ -0,0 +1,21 @@
+# Dictionary in which we will store a restaurat menu
+
+menu = {
+    "pizza":200,
+    "burger":150,
+    "hot_burgrt":180
+}
+
+# To add iten in menu
+menu["non-veg"] = 250
+
+# modify the menu price of the particular item
+menu["pizza"] = 220
+
+# If we want ot double the current price
+menu["burger"] = menu["burger"] * 2
+
+# To remove the item
+menu.pop("non-veg")
+
+print(menu)
