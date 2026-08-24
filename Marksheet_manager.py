49f78d31212746154913089e81fa8d2266d44e1c
Aditya yadav <adityayadav76430@gmail.com>
2026-08-24 13:57:08 +0530

    modified menu

--- Marksheet_manager.py
+++ Marksheet_manager.py
@@ -21,22 +21,6 @@ students["02"] = {
         "Science": 12
     }
 }
-
-# Options / student_menu .
-
-def menu():
-    print("\n======== STUDENT MANAGEMENT SYSTEM ========")
-    print("1. Add Student")
-    print("2. Search Student")
-    print("3. View Student")
-    print("4. Update Student")
-    print("5. Calculate Toatal Marks")
-    print("6. Calculate Total Percentage")
-    print("7. Calcualte Grade")
-    print("8. Result (pass/Fall)")
-    print("9. Load Data")
-    print("10. Save Data")
-    
     
 # creating a add_student dunction where user can add the student data
 
@@ -115,4 +99,15 @@ def calculate_percentage():
     else:
         print("In valid Id or student not found in record!")     
      
-
+def menu():
+    print("\n======== STUDENT MANAGEMENT SYSTEM ========")
+    print("1. Add Student")
+    print("2. Search Student")
+    print("3. View Student")
+    print("4. Update Student")
+    print("5. Calculate Toatal Marks")
+    print("6. Calculate Total Percentage")
+    print("7. Calcualte Grade")
+    print("8. Result (pass/Fall)")
+    print("9. Load Data")
+    print("10. Save Data")
